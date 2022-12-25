1175c8c6f4af0b19e490ebd9a4c8ff7d7500c888
sreddyiitr <sunilleo@gmail.com>
2022-12-25 15:38:30 -0600

    Update CalendarExample.java

--- samples/calendar/CalendarExample.java
+++ samples/calendar/CalendarExample.java
@@ -1,1 +1,16 @@
 
+package com.sunil.collections;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class CalendarExample {
+
+	public static void main(String[] args) {
+		TimeZone timeZone = TimeZone.getTimeZone("India/Mumbai");
+		Calendar calendar = Calendar.getInstance(timeZone);
+		System.out.println("Current Time: " + calendar.getTime());
+		System.out.println("Time Zone: " + calendar.getTimeZone().getDisplayName());
+	}
+
+}
