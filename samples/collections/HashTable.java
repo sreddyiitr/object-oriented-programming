5703478f7e3caa4c0196ea8e0799c912d0bcb84a
sreddyiitr <sunilleo@gmail.com>
2022-12-25 13:45:33 -0600

    Update HashTable.java

--- samples/collections/HashTable.java
+++ samples/collections/HashTable.java
@@ -1,1 +1,21 @@
 
+package com.sunil.collections;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class HashTableExample {
+
+	public static void main(String[] args) {
+		Map<Integer, String> hashTable = new Hashtable<>();
+		hashTable.put(10,"Ten");
+		hashTable.put(20,"Twenty");
+		hashTable.put(30,"Thirty");
+		
+		
+		System.out.println("Hash Table: ");
+		for (Map.Entry<Integer, String> entry : hashTable.entrySet()) {
+			System.out.println(entry.getKey() + " : "+ entry.getValue());
+		}
+	}	
+}
